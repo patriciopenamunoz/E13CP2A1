5513ac3aa556b57eefa7826aa3362f9afefe08c6
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-03 21:48:51 -0300

    Add todos#show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,22 @@ class TodosController < ApplicationController
   def new
     @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    completed  = { txt: 'Completed',  bg: 'bg-success' }
+    unfinished = { txt: 'Unfinished', bg: 'bg-danger' }
+    @status = @todo.completed ? completed : unfinished
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
 end
