9273b8d655d437fe230e225050124adaca300fbd
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-09-05 18:54:25 -0300

    Add todos#update

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -23,6 +23,12 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
 
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
   private
 
   def todo_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post 'todos', to: 'todos#create'
   get 'todos/:id', to: 'todos#show', as: 'todo'
   get 'todos/:id/edit', to: 'todos#edit', as: 'todos_edit'
+  patch 'todos/:id', to: 'todos#update'
 end
